9e6e1a4cfcdb5aca68573198995c95a07e0e1c85
lixiaoqing <lixiaoqing03@kuaishou.com>
2021-03-22 10:45:32 +0800

    x

---
+++ js/a.js
@@ -0,0 +1,11 @@
+/**
+ * csss
+ * 
+ */
+export default function a() {
+    console.log(123);
+
+    console.log(b)
+
+    var b=  'dd';
+}
--- js/main.js
+++ js/main.js
@@ -4,4 +4,7 @@
  */
 (function (globawl) {
     const { document } = globawl;
+	console.log('ddd');
+	var d = 1444;
+	console.log('a',a )
 }(this));
